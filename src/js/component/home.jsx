af1e41c7a7afedd11125fff00448091dbd525e1e
Aleco112 <aleco112@yahoo.fr>
2022-04-07 00:54:19 +0000

    new-fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 
@@ -6,6 +6,21 @@ import React, { useState } from "react";
 const Home = () => {
 	const [listname, setListName] = useState("");
 	const [todolist, setTodoList] = useState([]);
+	useEffect(() => {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Aleco112",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => setTodoList(result))
+			.catch((error) => console.log("error", error));
+	}, []);
+	console.log(todolist);
 
 	return (
 		<>
@@ -13,25 +28,48 @@ const Home = () => {
 				<input
 					type="text"
 					className="form-control"
-					placeholder="Recipient's listname"
+					placeholder="Todo-list"
 					onChange={(event) => {
 						setListName(event.target.value);
 					}}
 					value={listname}
+					onKeyUp={(e) => {
+						if (e.key == "Enter" && listname !== "") {
+							setTodoList([...todolist, listname]);
+							setListName("");
+						}
+					}}
 				/>
 				<button
 					onClick={() => {
-						setTodoList([...todolist, listname]);
-						setListName("");
+						//check is input is empty
+						if (listname !== "") {
+							setTodoList([...todolist, listname]);
+							setListName("");
+						}
 					}}
 					className="btn btn-outline-secondary"
 					type="button">
-					Add Name
+					Add task
 				</button>
 			</div>
 			<ul>
 				{todolist.map((item, index) => {
-					return <li key={index}>{item}</li>;
+					return (
+						<li key={index}>
+							{item.label}
+							<button
+								onClick={() =>
+									setTodoList(
+										todolist.filter((item, i) => {
+											return index !== i;
+										})
+									)
+								}>
+								X
+							</button>
+						</li>
+					);
 				})}
 			</ul>
 		</>
